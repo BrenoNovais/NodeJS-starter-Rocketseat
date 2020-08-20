446952660b704d8ed9b0b18e210ec57b4ef53454
Breno <breno.novais@hotmail.com>
2020-08-20 16:19:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// iniciando o APP
+const app = express();
+app.use(express.json());
+app.use(cors)
+
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/noseapi',
+    { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+
+app.listen(3001);
